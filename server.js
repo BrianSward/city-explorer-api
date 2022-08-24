4e04babda5b1d274d37f56cf43c350d71462c9c1
Brian Sward <bsward@gmail.com>
2022-08-23 22:20:47 -0700

    day 1 server workmare

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ console.log('first server');
 //bring in express and other requires
 const express = require('express');
 require('dotenv').config();
-// let data = require('./data/weather.json');
+let data = require('./data/weather.json');
 const cors = require('cors');
 
 
@@ -17,23 +17,27 @@ const PORT = process.env.PORT || 3002;
 //routes
 //routes proof of life
 
-// app.get('/',(request, response => {
-//   response.status(200).send('welcome to our server');
-// }));
+app.get('/', (request, response) => {
+  response.status(200).send('Welcome to our server');
+});
 
-// app.get('/weather', (request, response)=>{
-//   let city_name = request.query.city;
-//   let dataToGroom = data.find(city => city.city_name === city_name);
-//   let dataToSend = new City (dataToGroom);
-//   response.status(200).send(dataToSend);
-// });
+app.get('/weather', (request, response)=>{
+  // let city_name = request.query.city;
+  let dataToGroom = data.find(city => city.city_name.toLowerCase() === request.query.city_name.toLowerCase());
+  let dataToSend = new City (dataToGroom);
+  response.status(200).send(dataToSend);
+});
 
-// class City {
-//   constructor(cityObj){
-//     this.city_name = cityObj.city_name;
-//     // this.weather to go here
-//   }
-// }
+class City {
+  constructor(cityObj){
+    this.city_name = cityObj.city_name;
+    this.lon = cityObj.lon;
+    this.lat = cityObj.lat;
+    this.weather1 = cityObj.data[0].weather.description;
+    this.weather2 = cityObj.data[1].weather.description;
+    this.weather3 = cityObj.data[2].weather.description;
+  }
+}
 
 // catch all at bottom
 app.get('*', (request, response)=>{
