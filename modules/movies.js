0eaf68bb50df4d7dff40fcc391833290f8bf32bf
Brian Sward <bsward@gmail.com>
2022-08-26 18:26:46 -0700

    friday updates

--- modules/movies.js
+++ modules/movies.js
@@ -1,5 +1,6 @@
 'use strict';
 const axios = require('axios');
+let cache = {};
 
 async function getMovies(request, response) {
   const region = request.query.city;
@@ -7,9 +8,27 @@ async function getMovies(request, response) {
   const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${region}`;
 
   try {
-    const movieResponse = await axios.get(url);
-    const movieArray = movieResponse.data.results.map(movie => new Movie(movie));
-    response.status(200).send(movieArray);
+
+    let key = region+'movies';
+
+    if (cache[key] &&(Date.now() - cache[key].timeStamp < 50000)){
+      
+      console.log('Cache hit, movies present');
+      response.status(200).send(cache[key].data);
+
+    } else{
+      console.log('Cache missed - no movies present');
+      const movieResponse = await axios.get(url);
+
+      const movieArray = movieResponse.data.results.map(movie => new Movie(movie));
+
+      cache[key] = {
+        timeStamp: Date.now(),
+        data: movieArray
+      };
+
+      response.status(200).send(movieArray);
+    }
   } catch (err) {
     console.log('Error! Message is: ', err);
     response.status(500).send(err);
